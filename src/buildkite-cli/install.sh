44e288b954af9779b03bc9402f5f55911a74533c
sean <sean.sype.simmons@gmail.com>
2024-02-13 22:22:09 -0500

    changing up the values
    Signed-off-by: sean <sean.sype.simmons@gmail.com>

--- src/buildkite-cli/install.sh
+++ src/buildkite-cli/install.sh
@@ -1,3 +1,6 @@
+
+set -e
+
 . ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
@@ -5,13 +8,16 @@
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.5.4"
+ensure_nanolayer nanolayer_location "v0.5.6"
 
 
+
+
+release_tag_regex=$(if ! [ -z $RELEASETAGREGEX ]; then echo --release-tag-regex "${RELEASETAGREGEX}" ; fi)
+asset_regex_cmd=$(if ! [ -z $ASSETREGEX ]; then echo --asset-regex "${ASSETREGEX}" ; fi)
+lib_name_cmd=$(if ! [ -z $LIBNAME ]; then echo --lib-name "${LIBNAME}" ; fi)
 $nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.23" \
-    --option repo='buildkite/cli' --option binaryNames='cli-darwin-amd64,cli-darwin-arm64,cli-linux-amd64,cli-linux-arm64,cli-windows-amd64' --option version=$VERSION
-
-echo 'Done!'
+   install gh-release \
+   "$REPO" \
+   "$BINARYNAMES" --force \
+   --version "$VERSION" --bin-location "$BINLOCATION"  --lib-location "$LIBLOCATION"  $asset_regex_cmd $lib_name_cmd $release_tag_regex $ADDITIONALFLAGS
